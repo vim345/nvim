a0cfc7c88b061c4972cfafa9cbeafea30631e8b7
Mohammad Mohtasham <fake_email@gmail.com>
2024-01-10 23:16:48 -0800

    Add yaml and remove shift v weirdness

--- lua/vim345/core/remap.lua
+++ lua/vim345/core/remap.lua
@@ -1,7 +1,7 @@
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z")
 
 -- Maping shortcut keys to tab switching
 vim.keymap.set("n", "<C-l>", ":tabnext<CR>", { silent = true })
